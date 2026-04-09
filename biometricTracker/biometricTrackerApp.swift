cb3a3922b2cc307c2cbb01e6ff2206b92873762e
PrishaMarp <prishamarpu07@gmail.com>
2026-04-09 16:22:25 -0400

    Initial Commit

---
+++ biometricTracker/biometricTrackerApp.swift
@@ -0,0 +1,33 @@
+//
+//  biometricTrackerApp.swift
+//  biometricTracker
+//
+//  Created by Prisha Marpu on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct biometricTrackerApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
---
+++ biometricTrackerTests/biometricTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  biometricTrackerTests.swift
+//  biometricTrackerTests
+//
+//  Created by Prisha Marpu on 4/9/26.
+//
+
+import Testing
+@testable import biometricTracker
+
+struct biometricTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
